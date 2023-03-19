a434656114aa7db99e572552882d0253d1efbb57
Atthie Mulongo <atthie27@gmail.com>
2023-03-19 21:40:05 +0100

    stylisation du menu

--- src/menu.jsx
+++ src/menu.jsx
@@ -1,11 +1,17 @@
-function Menu() {
+import './App.css';
+function Menu(props) {
     return (
-        <div class="card" style="width: 18rem;">
-            <img class="card-img-top" src="..." alt="Card image cap"/>
+        <div class="card">
+            <img class="card-img-top" src={props.image} />
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div className='nom'>
+                    <h5 class="card-title">{props.nom}</h5>
+                    <p class="card-text">{props.description}</p>
+                    <h4 class="card-title">{props.prix}</h4>
+                </div>
+               
+               
+                <a href="#" class="btn btn-primary">Commander</a>
             </div>
         </div>
     )
