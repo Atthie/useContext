ec2d2c3f3a658481fb9e1c3dcf5ba4687db1f4f8
Atthie Mulongo <atthie27@gmail.com>
2023-03-19 20:08:10 +0100

    Stylisation de la navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,26 +5,31 @@ function Navbar() {
        
         <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid d-flex mb-3">
-                <div>
-                    <a class="navbar-brand me-auto p-2" href="#">Cake</a>
+                <div className="logo">
+                    <a class="navbar-brand me-auto p-2" href="#">MonMidi</a>
                 </div>
-                
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <div>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse p-2" id="navbarNav">
                         <ul class="navbar-nav ">
-                            <li class="nav-item panier">
-                                <img src= {process.env.PUBLIC_URL + 'panier.png'} />
-                                <h4>3</h4>
+                           
+                            <li class="nav-item">
+                                <a href="#">Acceuil</a>
                             </li>
                             <li class="nav-item">
+                                <a href="#">Connexion</a>
+                            </li>
+                            <li class="nav-item user">
                                 <img src= {process.env.PUBLIC_URL + 'utilisateur.png'} />
-                                <h4>Atthie</h4>
+                                <p>Atthie</p>
                             </li>
                             
                         </ul>
                     </div>
+                </div>
+                    
                 
 
             </div>
