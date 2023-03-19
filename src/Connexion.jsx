af30d3ef319588c79de785b73b9afde79c7201fd
Atthie Mulongo <atthie27@gmail.com>
2023-03-19 23:42:16 +0100

    creation de la function login

--- src/Connexion.jsx
+++ src/Connexion.jsx
@@ -1,18 +1,33 @@
 import Navbar from "./Navbar"
+import { useRef, useContext, useState } from "react";
+
 function Connexion(){
+    const loginRef = useRef('')
+    const [logins, setLogin] = useState([])
+
+    function login ( event ){
+        event.preventDefault()
+            console.log(logins)
+            const value = loginRef.current.value;
+            if (logins.includes(value)) 
+            {
+                console.log('deja')
+            } else {
+                setLogin([...logins, value]);
+                console.log('nouveau')
+            }
+            
+     }
     return (
         <div>
             <Navbar></Navbar>
             <div className="form">
-                <form>
+                <form onSubmit={login}>
                     <h3>Connexion</h3>
-                    <div class="form-group">
-                        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                    <div className="form-group">
+                        <input type="email"  ref={loginRef} className="form-control" id="exampleInputEmail1" placeholder="Enter email"/>
                     </div>
-                    <div class="form-group">
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
+                    <button type="submit" className="btn btn-primary">Submit</button>
                 </form>
             </div>
             
